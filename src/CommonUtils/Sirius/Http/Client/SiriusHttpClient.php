3e6d56dae1401111e526fd49c9b939299cfb19df
Chris Riley <chris.riley@digital.justice.gov.uk>
2017-07-21 17:49:59 +0100

    DAPH-67: Added additional logging into the http client

--- src/CommonUtils/Sirius/Http/Client/SiriusHttpClient.php
+++ src/CommonUtils/Sirius/Http/Client/SiriusHttpClient.php
@@ -5,9 +5,12 @@ namespace CommonUtils\Sirius\Http\Client;
 use CommonUtils\Sirius\Logging\Logger;
 use Traversable;
 use Zend\Http\Client as ZendHttpClient;
+use Zend\Http\Request;
 
 class SiriusHttpClient extends ZendHttpClient
 {
+    const REQUEST_LOG_MESSAGE = 'Making HTTP request: %s %s';
+    const RESPONSE_LOG_MESSAGE = '%s Response received: %s %s (%s bytes)';
     /** @var Logger $logger */
     private $logger;
 
@@ -40,5 +43,35 @@ class SiriusHttpClient extends ZendHttpClient
     {
         $this->logger = $logger;
         return $this;
+    }
+
+    public function send(Request $request = null)
+    {
+        $preRequest = microtime(true);
+
+        $this->logger->info(
+            sprintf(self::REQUEST_LOG_MESSAGE, $this->getMethod(), $this->getUri()->toString()),
+            ['category' => 'HTTP_CLIENT']
+        );
+
+        $response = parent::send($request);
+
+        $contentType = 'content-type/unknown';
+        if ($response->getHeaders()->has('Content-Type')) {
+            $contentType = $response->getHeaders()->get('Content-Type');
+        }
+
+        $this->logger->info(
+            sprintf(
+                self::RESPONSE_LOG_MESSAGE,
+                $contentType,
+                $response->getStatusCode(),
+                $response->getReasonPhrase(),
+                strlen($response->getContent())
+            ),
+            ['category' => 'HTTP_CLIENT', 'sub_request_time' => microtime(true) - $preRequest]
+        );
+
+        return $response;
     }
 }
