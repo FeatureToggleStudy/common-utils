f8f7c0835731b424bec3940dcb0d419a56bdb216
Vítor Brandão <vitor@noiselabs.io>
2017-09-14 09:59:50 +0100

    DAPH-180 Fix CustomJsonTest
    Signed-off-by: Vítor Brandão <vitor@noiselabs.io>

--- tests/CommonUtilsTest/Sirius/Logging/Format/CustomJsonTest.php
+++ tests/CommonUtilsTest/Sirius/Logging/Format/CustomJsonTest.php
@@ -1,17 +1,15 @@
 <?php
 
-namespace CommonUtilsTest\Sirius\Logging;
+namespace CommonUtilsTest\Sirius\Logging\Format;
 
 use CommonUtils\Sirius\Logging\Format\CustomJson;
+use PHPUnit_Framework_TestCase;
 
 /**
- * Class ExtractorTest
- *
- * @package CommonUtilsTest\Sirius\Logging\Format
+ * Class ExtractorTest.
  */
-class CustomJsonTest extends \PHPUnit_Framework_TestCase
+class CustomJsonTest extends PHPUnit_Framework_TestCase
 {
-
     public function setUp()
     {
         $this->customjson = new CustomJson();
@@ -26,7 +24,8 @@ class CustomJsonTest extends \PHPUnit_Framework_TestCase
         $event['extra'] = array('status_code' => 200, 'request_method' => 'POST');
         $result = $this->customjson->format($event);
 
-        $jsonResult = sprintf('{"timestamp":%s,"@fields":{"priority":7,"priorityName":"DEBUG","message":"Some logging test","status_code":200,"request_method":"POST"}}',$event['timestamp']->getTimestamp());
+        $jsonResult = sprintf('{"timestamp":%s,"priority":7,"priorityName":"DEBUG","message":"Some logging test","status_code":200,"request_method":"POST"}',
+            $event['timestamp']->getTimestamp());
 
         $this->assertEquals($jsonResult, $result);
     }
