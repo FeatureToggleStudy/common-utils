16dd8e959e29c025150034a8eaa0937b7f624ff2
Vítor Brandão <vitor@noiselabs.io>
2019-04-09 09:41:15 +0100

    SW-373 Remove backend specific feature toggles

--- src/Sirius/FeatureToggle/FeatureToggles.php
+++ src/Sirius/FeatureToggle/FeatureToggles.php
@@ -5,23 +5,6 @@ namespace CommonUtils\Sirius\FeatureToggle;
 
 class FeatureToggles
 {
-    const SCHEDULER_NAME = 'scheduler';
-    const SCHEDULER_DESC = 'Scheduler to process domain events';
-    const SCHEDULER_ON_MSG = 'Scheduler is running';
-    const SCHEDULER_OFF_MSG = 'Scheduler is currently disabled (feature toggle is off)';
-
-    const PUBLIC_API_NAME = 'public_api';
-    const PUBLIC_API_DESC = 'Public API endpoints for systems integration';
-    const PUBLIC_API_ON_MSG = 'Public API endpoints are available';
-    const PUBLIC_API_OFF_MSG = 'Public API endpoints are currently disabled and unavailable (feature toggle is off)';
-
-    const GENERATE_COURT_REFERENCE_NAME = 'generate_court_reference';
-    const GENERATE_COURT_REFERENCE_DESC = "Generate valid court reference when '00000000' is used";
-    const GENERATE_COURT_REFERENCE_ON_MSG = "Generate valid court reference when '00000000' is used is available";
-    const GENERATE_COURT_REFERENCE_OFF_MSG = "Generate valid court reference when '00000000' is used is currently disabled (feature toggle is off)";
-
-    const CLIENT_SOURCE_FIELD_NAME = 'client_source_field';
-
     const FLAG_STATUS_ACTIVE = 'always-active';
     const FLAG_STATUS_INACTIVE = 'inactive';
     const FLAG_STATUS_CONDITIONAL = 'conditionally-active';
